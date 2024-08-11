aeff73c576f52bf6630a40e2a67a6ab7caa8aca4
Akash Kumar Maurya <107799029+Akash7892@users.noreply.github.com>
2024-08-11 13:31:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def recommend(movie):
 
 st.header('Movie Recommendation System Using Machine Learning')
 movies = pickle.load(open('artifacts/movie_list.pkl','rb'))
-similarity = pickle.load(open('artifacts/similarity.pkl','rb'))
+similarity = pickle.load(open('artifacts/similarity.pkl.gz','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
